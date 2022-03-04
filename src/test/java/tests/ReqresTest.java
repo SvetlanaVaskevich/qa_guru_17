5ece967c0cb9318d454f91d654529d1f6fad18e9
Svetlana Vaskevich <svaskevich@bk.ru>
2022-03-04 22:47:25 +0500

    Updates import

--- src/test/java/tests/ReqresTest.java
+++ src/test/java/tests/ReqresTest.java
@@ -1,17 +1,10 @@
 package tests;
 
-import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Map;
-
-import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class ReqresTest {
